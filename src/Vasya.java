db968716ea548a5067b0f754611deeaa36192b9c
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-12-21 16:06:01 +0300

    Update Vasya.java

--- src/Vasya.java
+++ src/Vasya.java
@@ -1,41 +1,19 @@
 
 public class Vasya {
-
     public static void main(String[] args) {
-
         String text = "Вася заработал 50 рублей, Петя - 7563 рубля, а Маша - 33789 рублей";
-
-
-
         StringBuilder result = new StringBuilder();
         StringBuilder result1 = new StringBuilder();
-
         //Считаем Васю
         for (int i = 0; i < text.indexOf(","); i++)
             if (text.charAt(i) >= '0' && text.charAt(i) <= '9')
                 result.append(text.charAt(i));
-
-
-
         //Считаем Машу
         for (int i = text.indexOf("Маша"); i < text.length(); i++)
             if (text.charAt(i) >= '0' && text.charAt(i) <= '9')
                 result1.append(text.charAt(i));
-
-
-
         int sum;
-
         sum = Integer.parseInt(String.valueOf(result1)) + Integer.parseInt(String.valueOf(result));;
-
         System.out.println(sum);
-
-
-
-
-
-
-
     }
-
 }
